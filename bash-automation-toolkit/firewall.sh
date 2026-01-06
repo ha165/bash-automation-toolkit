147ad478d8c904b323fe49a54bc2f9c719f66267
ha165 <lumumbaharmony@gmail.com>
2026-01-06 07:36:12 +0300

    Firewall.sh

---
+++ bash-automation-toolkit/firewall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#FIREWALL AUTOMATION
+
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw enable
+echo "UFW configured"
